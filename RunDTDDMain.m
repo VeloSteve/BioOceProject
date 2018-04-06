347ac83bf6f2adc2f7d37fe608b66c893e160688
Steve Ryan <1319496+VeloSteve@users.noreply.github.com>
2018-04-06 15:43:40 -0700

    Standardize format and spelling, use vectors for speed
    At new lines 18 and 31-34 vector math provides a substantial speedup,

---
+++ RunDTDDMain.m
@@ -0,0 +1,10 @@
+
+% Call in GUI:
+%  [Rt,Mu,Sig]=DTDDMain(n,HH,MM,Kz,irr,maxZ);
+% and the signature is
+% function [Rt,Mu,Sig] = DTDDMain(n,HH,MM,Kz,irr,maxZ)
+
+% irr is a 1x721 array in the default state.
+% 721 = 1 + 60*12
+irr = irrCall_noGUI();
+DTDDMain(4, 18, 0, 10, irr, 100);
