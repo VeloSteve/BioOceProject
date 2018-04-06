ea8c878abcce53eb66f8909ee664c4d940df75e6
Steve Ryan <1319496+VeloSteve@users.noreply.github.com>
2018-04-06 15:47:59 -0700

    Revert "Revert "Revert "Standardize format and spelling, use vectors for speed"""
    This reverts commit e6dbe25be7458ff522f1f44cc96af880d8a59699.

--- DepthArray.m
+++ DepthArray.m
@@ -1,4 +1,4 @@
-function [z] = DepthArray(n, dt, nt, Kz, maxZ)
+function [z] = DepthArray(n,dt,nt,Kz,maxZ)
 %DepthArray This function is used to reduce the memory load on the
 %Nicks_DTDD script.  It takes in the number of time steps and the n value
 %(10^n) which determines how many cells we will be dealing with and returns
@@ -13,26 +13,35 @@ function [z] = DepthArray(n, dt, nt, Kz, maxZ)
 %       z - a 10^n x nt matrix of the depth experienced by each cell for each
 %       step in time
 
-    z = zeros(10^n, nt); % holding vector for our depth values. 
+    z = zeros(10^n,nt); % holding vector for our depth values. 
 
-    z(:, 1) = linspace(0, maxZ, 10^n);
+    for i = 2:10^n
+        z(i) = z(i-1) + (maxZ/10^n); % evenly spacing our initial depth from 0 to ~100m
+    end
+    
     walk = sqrt(2*dt*Kz); % this is the distance that will be walked,
                           % either up or down based on our timestep and
-                          % vertical eddy diffusivity
+                          % verctical eddy difusivity
 % Now that we have our starting depths, we perform our random walk
     for i = 2:nt % time step loop
-        pm = sign(0.50-rand(10^n,1)); % a matrix of randomly generated positive
+        pm = sign(0.51-rand(10^n,1)); % a matrix of randomly generated positve
                                    % or negative ones that will the random
                                    % part of our simulated walk.
         pm=pm.*walk;
         z(:,i)=z(:,i-1)+pm;
-        % Calculate boundaries in vector form.  1.147 seconds for n=4 becomes
-        % 0.16 seconds for the whole function.  
-        ltz = find(z(:, i) < 0);
-        z(ltz, i) = -z(ltz, i);
-        gtmax = find(z(:, i) > maxZ);
-        z(gtmax, i) = 2*maxZ - z(gtmax, i);
-                   
+        for j = 1:10^n
+            %z(j,i) = NickEq4(z(j,i-1),walk,pm(j,i));
+             %z(j,i) = z(j,i-1) + (pm(j)*walk);
+             % We need to code in our boundary conditions.  This is a relfective
+            % boundary to prevent cell accumulation at the edges.
+                if z(j,i) < 0 % phytoplankton don't generally take to the air
+                    z(j,i) = z(j,i)*-1;
+                elseif z(j,i) > maxZ % we aren't going to consider depths greater than 100m
+                    z(j,i) = maxZ -(z(j,i)-maxZ);
+                else % but if everything is okay, we proceed as planned.
+
+                end
+        end
       clear pm;
     end
 clear i; clear j; clear walk, clear pm;
