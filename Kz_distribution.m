8b702e52a41ae0bc92bdb23202bf6c42aa04ce2b
Steve Ryan <1319496+VeloSteve@users.noreply.github.com>
2018-04-10 20:15:39 -0700

    Create a depth-dependent diffusion array
    This is fairly arbitrary, but one part may be important.  Keeping the second derivative of K low is supposed to make the results more reliable.

--- Kz_distribution.m
+++ Kz_distribution.m
@@ -1,8 +1,13 @@
-function dist = Kz_distribution(Kbase, n)
-% Create a diffusivity distribution base on inputs.  FOR NOW this is
+function dist = Kz_distribution(Kbase, maxZ, n)
+% Create a diffusivity distribution based on inputs.  FOR NOW this is
 % just a fixed test distribution.
     dist = ones(n, 1)* Kbase;
-    dist(n/2:end) = Kbase/10;
-
-
+    % Decrease Kz linearly over a distance of 3 meters, starting at 50 meters.
+    % This is arbitrary, but by using a fixed distance the slope (and 2nd
+    % derivative) shouldn't be affected by the total number of points used.
+    tPoints = 3.0/(maxZ/n);
+    transition = linspace(Kbase, Kbase/10, tPoints);
+    pos = n*50/maxZ;
+    dist(pos:pos+tPoints-1) = transition;
+    dist(pos+tPoints:end) = Kbase/10;
 end
