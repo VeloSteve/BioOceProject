5be434a0c9e0bb61f009201cb9d513153489d3c0
Steve Ryan <1319496+VeloSteve@users.noreply.github.com>
2018-04-10 20:30:35 -0700

    Interpolate to actual time step
    Although dt is editable in the main program, this was hardwired.  Now it can change as needed.  Also fixed some spelling errors.

--- irrCall_noGUI.m
+++ irrCall_noGUI.m
@@ -1,4 +1,4 @@
-function [irr] = irrCall_quiet(source,eventdata )
+function [irr] = irrCall_noGUI(step_seconds )
 %IRRCALL_QUIET This function performs the same operation as the irrCall
 %function within the DTDDGui function.  This one simply does not produce a
 %plot as part of that.
@@ -19,9 +19,12 @@ irr=importdata('dayIrr.mat');
 % irr is a vector with solar irradiance values from 06:00 to 18:00 every
 % 5 minutes.  We are going to interpolate the 5 minute data onto a time
 % vector that is every 1 minute so we have the values needed to run this
-% siumulation.  For that we will need some time vectors
+% simulation.  For that we will need some time vectors
 oldT=datenum(2014,07,24,06,00,00):datenum(00,00,00,00,05,00):datenum(2014,07,24,18,00,00);
-newT=datenum(2014,07,24,06,00,00):datenum(00,00,00,00,01,00):datenum(2014,07,24,18,00,00);
+% Convert step_seconds to minutes and seconds.
+sM = floor(step_seconds / 60);
+sS = step_seconds - sM * 60;
+newT=datenum(2014,07,24,06,00,00):datenum(00,00,00,00,sM,sS):datenum(2014,07,24,18,00,00);
 irr=interp1(oldT,irr,newT,'spline');
 clear oldT;
 irr(irr<0)=0; % no such thing as negative irradiance
